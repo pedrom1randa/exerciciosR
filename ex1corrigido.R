6e1ad7d96127caf686ea4096f36b518830252cf8
profnssorg <profnssorg@gmail.com>
2017-11-24 20:11:13 -0200

    Add files via upload

---
+++ ex1corrigido.R
@@ -0,0 +1,44 @@
+# Aplicativo Exercicio1-1.R
+# Descrição: Este aplicativo soma números..
+# Autor: Nelson Seixas dos Santos
+# Data: 24/11/2017
+# Versão: 0.0.1
+
+# Instalação de pacotes
+
+
+# Importação de módulos
+
+
+
+# Definição de classes
+
+
+
+# Definição de funções
+
+
+
+# Declaração de variáveis
+
+
+
+
+# Entrada de dados
+
+print('Digite um numero inteiro e tecle "enter" duas vezes.')
+y <- scan()
+
+print('Digite o segundo  inteiro e tecle "enter" duas vezes.')
+x <- scan()
+
+
+# Processamento de dados
+
+soma <- x + y
+
+
+# Saída de dados
+
+print('A soma dos números é: ')
+print(x+y)
